d2a78c1ea90b3d859c8f67d056993c9f97206119
seanBr97 <brieffies@tcd.ie>
2018-03-29 12:44:29 +0100

    added notify method to listener

--- Kinectures/KinectListener.cs
+++ Kinectures/KinectListener.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Microsoft.Kinect;
 
 namespace Kinectures
 {
     public class KinectListener : IListener
     {
         public KinectListener()
+        {
+
+        }
+
+        public void notify (BodyFrameReference reference)
         {
 
         }
